d71ff256fa5e91c5866e4e4130fc7bc1fef3b436
Juann Strauss <Juann.Strauss@equitybank.co.ke>
2026-03-26 08:17:48 +0200

    ..

--- SimpleGateway.Api/Program.cs
+++ SimpleGateway.Api/Program.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using SimpleGateway.Api.Data;
 using System.Threading.Tasks;
+using Scalar.AspNetCore;
 
 namespace SimpleGateway.Api
 {
@@ -15,6 +16,7 @@ namespace SimpleGateway.Api
             ConfigureServices(builderMain);
             builderMain.WebHost.ConfigureKestrel(opts => opts.ListenAnyIP(8000));
             var appMain = builderMain.Build();
+            
             ConfigurePipeline(appMain);
 
             // Build admin app (port 8001)
@@ -22,6 +24,7 @@ namespace SimpleGateway.Api
             ConfigureServices(builderAdmin);
             builderAdmin.WebHost.ConfigureKestrel(opts => opts.ListenAnyIP(8001));
             var appAdmin = builderAdmin.Build();
+            
             ConfigurePipeline(appAdmin);
 
             // Run both apps
@@ -32,9 +35,7 @@ namespace SimpleGateway.Api
         {
             builder.Services.AddControllers();
             builder.Services.AddOpenApi();
-            // Add Swagger generator for UI and JSON endpoint
-            builder.Services.AddSwaggerGen();
-
+            
             builder.Services.AddHttpClient("singletonClient").SetHandlerLifetime(Timeout.InfiniteTimeSpan);
             builder.Services.AddSingleton(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("singletonClient"));
             builder.Services.AddSingleton<SimpleGateway.Api.Utils.HttpUtil>();
@@ -49,19 +50,7 @@ namespace SimpleGateway.Api
 
         private static void ConfigurePipeline(WebApplication app)
         {
-            // Enable Swagger UI and OpenAPI for both apps
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SimpleGateway API V1");
-                c.RoutePrefix = "swagger"; // UI at /swagger
-            });
-
-            // Keep MapOpenApi for the Microsoft OpenAPI endpoints in development
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
+            app.MapScalarApiReference();
 
             app.UseAuthorization();
             app.MapControllers();
